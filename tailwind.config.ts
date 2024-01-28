45afd185dac47ced74aa595260f4a8ae9369ebc7
Kelli Libin <km.putnam2@gmail.com>
2024-01-28 15:20:04 -0700

    delete bg images, add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        customDarkGreen: "#31402A",
+        customWhite: "#F2F2F2",
+        customLightGreen: "#B5CCAA",
+        customBeige: "#D9C2AD",
+        customBrown: "#A67E6F",
       },
     },
   },
