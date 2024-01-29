e6f81aaf9c72493057acaa11633bba7feadf4e9b
Kelli Libin <km.putnam2@gmail.com>
2024-01-29 16:49:33 -0700

    started second block, the who we are section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import cottage from "../../public/images/cottage.jpg";
 // import shanerounce from "../../public/images/shanerounce.jpg";
 import ybagmedia from "../../public/images/ybagmedia.jpg";
 // import cow from "../../public/images/cow.jpg";
@@ -8,31 +9,71 @@ import paths from "./paths";
 
 export default function Home() {
   return (
-    <div className="relative h-screen">
-      <div className="absolute inset-0 -z-10 ">
-        <Image
-          src={ybagmedia}
-          fill
-          style={{ objectFit: "cover" }}
-          alt="highlands cow"
-        />
-        <div className="absolute inset-0 bg-slate-800 bg-opacity-30"></div>
+    <main className="flex flex-col bg-customWhite">
+      {/* hero */}
+      <div className="relative h-screen">
+        <div className="absolute inset-0">
+          <Image
+            src={ybagmedia}
+            fill
+            style={{ objectFit: "cover" }}
+            alt="highlands cow"
+          />
+          <div className="absolute inset-0 bg-slate-800 bg-opacity-30"></div>
+        </div>
+
+        <div className="absolute bottom-[18%] right-[5%] text-center">
+          <h1 className={`${titleFont.className} text-6xl text-customWhite`}>
+            Quality Mountain-Raised Beef
+          </h1>
+          <h3
+            className={`${titleFont.className} mb-7 text-4xl text-customWhite`}
+          >
+            Naturally Organic, Uniquely Flavorful
+          </h3>
+          <Link
+            className={`${textFont.className} rounded bg-customBeige p-4`}
+            href={paths.farmBeefPath()}
+          >
+            Learn More
+          </Link>
+        </div>
       </div>
 
-      <div className="absolute bottom-[18%] right-[5%] text-center">
-        <h1 className={`${titleFont.className} text-6xl text-customWhite`}>
-          Quality Mountain-Raised Beef
-        </h1>
-        <h2 className={`${titleFont.className} mb-7 text-4xl text-customWhite`}>
-          Naturally Organic, Uniquely Flavorful
-        </h2>
-        <Link
-          className={`${textFont.className} rounded bg-customBeige p-4`}
-          href={paths.farmBeefPath()}
+      <div className=" bg-customWhite relative mx-auto flex w-4/5 items-center justify-center pb-[2rem]">
+        <h2
+          className={`${titleFont.className} absolute left-[5%] top-[-1rem] -translate-x-1/2 transform bg-customDarkGreen p-4 text-5xl text-customWhite`}
         >
-          Learn More
-        </Link>
+          Who We Are
+        </h2>
+        <div className="flex justify-center items-center mt-32  bg-customLightGreen rounded p-8">
+        <div className="w-1/3 rounded-[50%]">
+          <Image
+            src={cottage}
+            height={300}
+            width={300}
+            alt="our ranch"
+            className="rounded-[50%]"
+          />
+        </div>
+        <div className="w-3/5 flex flex-col items-end text-right">
+          <p className="p-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.
+          </p>
+          <Link
+            className={`${textFont.className} rounded bg-customBeige p-4 mt-4`}
+            href={paths.farmBeefPath()}
+          >
+            Read More
+          </Link>
+        </div>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
