9e5fe76d6fa60e4eccae66e1375cbdb07ac111e6
Kelli Libin <km.putnam2@gmail.com>
2024-01-29 15:30:07 -0700

    added path routes

---
+++ src/app/paths.ts
@@ -0,0 +1,25 @@
+const paths = {
+  homePath() {
+    return "/";
+  },
+  farmBeefPath() {
+    return "/farmbeef";
+  },
+  contactPath() {
+    return "/contact";
+  },
+  aboutPath() {
+    return "/jpel";
+  },
+  feedingPath() {
+    return "/ourcattle/feeding";
+  },
+  historyPath() {
+    return "/ourcattle/history";
+  },
+  studiesPath() {
+    return "/ourcattle/studies";
+  },
+};
+
+export default paths;
