84865e53344bb00bd4e29ab8ffc5ae70a7371cf4
Kelli Libin <km.putnam2@gmail.com>
2024-01-30 15:43:48 -0700

    on hover use medium green, scale 110

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,10 @@ import cottage from "../../public/images/cottage.jpg";
 import seregykotenev from "../../public/images/seregykotenev.jpg";
 import shanerounce from "../../public/images/shanerounce.jpg";
 import ybagmedia from "../../public/images/ybagmedia.jpg";
-// import cow from "../../public/images/cow.jpg";
 import { textFont, titleFont } from "../app/utils/fonts";
 import paths from "./paths";
 import Slider from "@/components/Slider";
 import { FaArrowAltCircleRight } from "react-icons/fa";
-
-// import { LiaHatCowboySideSolid } from "react-icons/lia";
 
 export default function Home() {
   return (
@@ -27,7 +24,7 @@ export default function Home() {
           <div className="absolute inset-0 bg-slate-800 bg-opacity-30"></div>
         </div>
 
-        <div className="absolute bottom-[18%] right-[5%] text-center flex flex-col items-center">
+        <div className="absolute bottom-[18%] right-[5%] flex flex-col items-center text-center">
           <h1 className={`${titleFont.className} text-6xl text-customWhite`}>
             Quality Mountain-Raised Beef
           </h1>
@@ -37,10 +34,11 @@ export default function Home() {
             Naturally Organic, Uniquely Flavorful
           </h3>
           <Link
-            className={`${textFont.className} flex items-center shadow-element-grey rounded bg-customBeige p-4 underline duration-200 hover:bg-customDarkGreen hover:text-customWhite active:scale-90`}
+            className={`${textFont.className} shadow-outline flex items-center rounded bg-customBeige p-4 underline duration-200 hover:scale-105 hover:bg-customMediumGreen hover:text-customWhite active:scale-90`}
             href={paths.farmBeefPath()}
           >
-            <p className="font-bold tracking-wider">Read More</p> <FaArrowAltCircleRight className="ml-2" />
+            <p className="font-bold tracking-wider">Read More</p>{" "}
+            <FaArrowAltCircleRight className="ml-2" />
           </Link>
         </div>
       </div>
@@ -53,8 +51,8 @@ export default function Home() {
         >
           Who We Are
         </h2>
-        <div className="shadow-element-grey my-[10rem] flex h-[100%] w-[90%] items-center  justify-center rounded bg-customBeige bg-opacity-50 p-6">
-          <div className="shadow-element flex h-[95%] w-[100%] items-center justify-center rounded bg-customLightGreen bg-opacity-50 p-4">
+        <div className="shadow-element-grey my-[10rem] flex h-[100%] w-[90%] items-center  justify-center rounded bg-customBrown bg-opacity-30 p-6">
+          <div className="shadow-element flex h-[95%] w-[100%] items-center justify-center rounded bg-customLightGreen bg-opacity-60 p-4">
             <div className="flex w-1/3 justify-start rounded-[50%]">
               <Image
                 src={cottage}
@@ -73,7 +71,7 @@ export default function Home() {
               </p>
               <Link
                 className={`${titleFont.className} shadow-element-grey mt-4 flex items-center rounded bg-customDarkGreen p-4 tracking-wider
-                 text-customWhite underline`}
+                text-customWhite underline duration-200 hover:scale-105 hover:bg-customMediumGreen active:scale-90`}
                 href={paths.aboutPath()}
               >
                 Learn More <FaArrowAltCircleRight className="ml-2" />
@@ -91,7 +89,7 @@ export default function Home() {
           The Highland Cow
         </h2>
 
-        <div className="relative flex h-full w-full items-center justify-end rounded bg-customLightGreen bg-opacity-50">
+        <div className="relative flex h-full w-full items-center justify-end rounded bg-customLightGreen bg-opacity-60">
           <Slider />
           <div className="flex h-full justify-end rounded-[50%] lg:w-[85%]">
             <Image
@@ -111,8 +109,8 @@ export default function Home() {
         >
           100% Free-Range, Grassfed Beef
         </h2>
-        <div className="shadow-element-grey my-[10rem] flex h-[100%] w-[90%] items-center  justify-center rounded bg-customBeige bg-opacity-50 p-6">
-          <div className="shadow-element flex h-[95%] w-[100%] items-center justify-center rounded bg-customLightGreen bg-opacity-50 p-4">
+        <div className="shadow-element-grey my-[10rem] flex h-[100%] w-[90%] items-center  justify-center rounded bg-customBrown bg-opacity-30 p-6">
+          <div className="shadow-element flex h-[95%] w-[100%] items-center justify-center rounded bg-customLightGreen bg-opacity-60 p-4">
             <div className="flex w-1/3 justify-start rounded-[50%]">
               <Image
                 src={seregykotenev}
@@ -132,8 +130,8 @@ export default function Home() {
                 incididunt ut labore et dolore magna aliqua.
               </p>
               <Link
-                className={`${titleFont.className} shadow-element-grey mt-4 flex items-center rounded bg-customDarkGreen p-4 tracking-wider
-                 text-customWhite underline duration-200 hover:bg-customBrown active:scale-90`}
+                className={`${titleFont.className} hover:scale-105 shadow-element-grey mt-4 flex items-center rounded bg-customDarkGreen p-4 tracking-wider
+                 text-customWhite underline duration-200 hover:bg-customMediumGreen active:scale-90`}
                 href={paths.farmBeefPath()}
               >
                 Learn More <FaArrowAltCircleRight className="ml-2" />
