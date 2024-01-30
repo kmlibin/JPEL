2869ea19e6affdbdbb3e5aaa3cddbc4679280dbb
Kelli Libin <km.putnam2@gmail.com>
2024-01-30 11:54:32 -0700

    remove code, put in new slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import ybagmedia from "../../public/images/ybagmedia.jpg";
 // import cow from "../../public/images/cow.jpg";
 import { textFont, titleFont } from "../app/utils/fonts";
 import paths from "./paths";
+import Slider from "@/components/Slider";
+import { MdArrowLeft, MdArrowRight } from "react-icons/md";
 
 export default function Home() {
   return (
@@ -76,7 +78,7 @@ export default function Home() {
       </div>
 
       {/* fun facts section */}
-      <div className=" relative flex w-full items-center justify-center bg-customLightGreen md:min-h-[100vh] lg:min-h-[70vh]">
+      <div className=" relative flex w-full items-center justify-center bg-customLightGreen lg:h-[110vh] xlscreen">
         <h2
           className={`${titleFont.className} absolute right-[2%] top-[-1rem] z-10 bg-customDarkGreen p-12 text-5xl text-customWhite`}
         >
@@ -84,26 +86,8 @@ export default function Home() {
         </h2>
 
         <div className="relative flex h-full w-full items-center justify-end rounded bg-customLightGreen">
-          <div className="left-[7%] top-1/2 transform -translate-y-1/2 absolute rounded w-1/3 bg-customDarkGreen bg-opacity-80 p-5">
-            <div className=" flex min-h-full w-full flex-col items-start  text-left">
-              <p
-                className={`${textFont.className} min-w-full rounded bg-customBrown p-5 text-3xl`}
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit.
-              </p>
-              <Link
-                className={`${titleFont.className} mt-4 rounded bg-customBeige p-4`}
-                href={paths.farmBeefPath()}
-              >
-                Read More
-              </Link>
-            </div>
-          </div>
-          <div className="flex h-full md:w-4/5 lg:w-2/3 justify-end rounded-[50%]">
+          <Slider />
+          <div className="flex h-full lg:w-[85%] justify-end rounded-[50%]">
             <Image src={shanerounce} className="h-[100%]" style={{ objectFit: "cover" }} alt="our ranch" />
           </div>
         </div>
