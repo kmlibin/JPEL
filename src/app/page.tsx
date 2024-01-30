c90c12140d0ead0d1edae397174bfefe42b1ee3a
Kelli Libin <km.putnam2@gmail.com>
2024-01-30 14:42:11 -0700

    border to lgboxes on top and bottom now responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,32 +53,32 @@ export default function Home() {
         >
           Who We Are
         </h2>
-        <div className="shadow-element-grey flex w-5/6 h-[52vh] my-[10rem] items-center justify-center bg-customBeige bg-opacity-30">
-        <div className="shadow-element my-40 flex w-[95%] max-h-[90%] items-center  justify-center rounded bg-customLightGreen bg-opacity-50 p-8">
-          <div className="flex w-1/3 justify-start rounded-[50%]">
-            <Image
-              src={cottage}
-              height={350}
-              width={350}
-              alt="our ranch"
-              className="shadow-element rounded-[50%] border border-customDarkGreen"
-            />
+        <div className="shadow-element-grey my-[10rem] flex h-[100%] w-[90%] items-center  justify-center rounded bg-customBeige bg-opacity-50 p-6">
+          <div className="shadow-element flex h-[95%] w-[100%] items-center justify-center rounded bg-customLightGreen bg-opacity-50 p-4">
+            <div className="flex w-1/3 justify-start rounded-[50%]">
+              <Image
+                src={cottage}
+                height={350}
+                width={350}
+                alt="our ranch"
+                className="shadow-element rounded-[50%] border border-customDarkGreen"
+              />
+            </div>
+            <div className="flex w-3/5 flex-col items-end text-right">
+              <p className={`${textFont.className} p-4 text-3xl`}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+              <Link
+                className={`${titleFont.className} mt-4 flex items-center rounded bg-customBeige p-4`}
+                href={paths.farmBeefPath()}
+              >
+                Read More <FaArrowAltCircleRight className="ml-2" />
+              </Link>
+            </div>
           </div>
-          <div className="flex w-3/5 flex-col items-end text-right">
-            <p className={`${textFont.className} p-4 text-3xl`}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <Link
-              className={`${titleFont.className} mt-4 flex items-center rounded bg-customBeige p-4`}
-              href={paths.farmBeefPath()}
-            >
-              Read More <FaArrowAltCircleRight className="ml-2" />
-            </Link>
-          </div>
-        </div>
         </div>
       </div>
 
@@ -104,14 +104,14 @@ export default function Home() {
       </div>
 
       {/* buy beef section */}
-      <div className="relative mx-auto flex w-full h-full items-center justify-center bg-customWhite">
+      <div className="relative mx-auto flex h-full w-full items-center justify-center bg-customWhite">
         <h2
           className={`${titleFont.className} shadow-element absolute left-[2%] top-[-1rem] ml-[24rem] -translate-x-1/2 transform bg-customDarkGreen p-12 text-5xl text-customWhite`}
         >
           100% Free-Range, Grassfed Beef
         </h2>
-        <div className="shadow-element-grey flex w-5/6 h-[52vh] my-[10rem] items-center justify-center bg-customBeige bg-opacity-30">
-          <div className="shadow-element my-40 flex w-[95%] max-h-[90%] items-center justify-center rounded bg-customLightGreen bg-opacity-50 p-8">
+        <div className="shadow-element-grey my-[10rem] flex h-[100%] w-[90%] items-center  justify-center rounded bg-customBeige bg-opacity-50 p-6">
+          <div className="shadow-element flex h-[95%] w-[100%] items-center justify-center rounded bg-customLightGreen bg-opacity-50 p-4">
             <div className="flex w-1/3 justify-start rounded-[50%]">
               <Image
                 src={seregykotenev}
@@ -131,7 +131,8 @@ export default function Home() {
                 incididunt ut labore et dolore magna aliqua.
               </p>
               <Link
-                className={`${titleFont.className} mt-4 flex items-center rounded bg-customBeige p-4`}
+                className={`${titleFont.className} mt-4 flex items-center rounded bg-customDarkGreen p-4 text-customWhite
+                 duration-200 hover:bg-customBrown active:scale-90`}
                 href={paths.farmBeefPath()}
               >
                 Learn More <FaArrowAltCircleRight className="ml-2" />
