320f89572fa8510dd90458ff2629d4ed6c68439a
Kelli Libin <km.putnam2@gmail.com>
2024-01-29 20:54:00 -0700

    styled interesting facts div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import cottage from "../../public/images/cottage.jpg";
-// import shanerounce from "../../public/images/shanerounce.jpg";
+import shanerounce from "../../public/images/shanerounce.jpg";
 import ybagmedia from "../../public/images/ybagmedia.jpg";
 // import cow from "../../public/images/cow.jpg";
 import { textFont, titleFont } from "../app/utils/fonts";
@@ -48,7 +48,7 @@ export default function Home() {
         >
           Who We Are
         </h2>
-        <div className="my-44 flex w-4/5 items-center justify-center  rounded bg-customLightGreen p-8">
+        <div className="my-40 flex w-4/5 items-center justify-center  rounded bg-customLightGreen p-8">
           <div className="flex w-1/3 justify-start rounded-[50%]">
             <Image
               src={cottage}
@@ -59,13 +59,11 @@ export default function Home() {
             />
           </div>
           <div className="flex w-3/5 flex-col items-end text-right">
-            <p className="p-4">
+            <p className={`${textFont.className} p-4 text-3xl`}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua.
+              nisi ut aliquip ex ea commodo consequat.
             </p>
             <Link
               className={`${titleFont.className} mt-4 rounded bg-customBeige p-4`}
@@ -78,38 +76,35 @@ export default function Home() {
       </div>
 
       {/* fun facts section */}
-      <div className=" relative mx-auto flex w-full items-center justify-center bg-customBrown">
+      <div className=" relative flex w-full items-center justify-center bg-customLightGreen md:min-h-[100vh] lg:min-h-[70vh]">
         <h2
-          className={`${titleFont.className} absolute right-[2%] top-[-1rem] bg-customDarkGreen p-12 text-5xl text-customWhite`}
+          className={`${titleFont.className} absolute right-[2%] top-[-1rem] z-10 bg-customDarkGreen p-12 text-5xl text-customWhite`}
         >
           Interesting Facts
         </h2>
 
-        <div className="my-44 flex w-4/5 items-center justify-center rounded bg-customLightGreen p-8">
-          <div className="flex w-3/5 flex-col items-start text-left">
-            <p className={`${textFont.className} text-2xl`}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua.
-            </p>
-            <Link
-              className={`${titleFont.className} mt-4 rounded bg-customBeige p-4`}
-              href={paths.farmBeefPath()}
-            >
-              Read More
-            </Link>
+        <div className="relative flex h-full w-full items-center justify-end rounded bg-customLightGreen">
+          <div className="left-[7%] top-1/2 transform -translate-y-1/2 absolute rounded w-1/3 bg-customDarkGreen bg-opacity-80 p-5">
+            <div className=" flex min-h-full w-full flex-col items-start  text-left">
+              <p
+                className={`${textFont.className} min-w-full rounded bg-customBrown p-5 text-3xl`}
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit.
+              </p>
+              <Link
+                className={`${titleFont.className} mt-4 rounded bg-customBeige p-4`}
+                href={paths.farmBeefPath()}
+              >
+                Read More
+              </Link>
+            </div>
           </div>
-          <div className="flex w-1/3 justify-end rounded-[50%]">
-            <Image
-              src={cottage}
-              height={300}
-              width={300}
-              alt="our ranch"
-              className="rounded-[50%]"
-            />
+          <div className="flex h-full md:w-4/5 lg:w-2/3 justify-end rounded-[50%]">
+            <Image src={shanerounce} className="h-[100%]" style={{ objectFit: "cover" }} alt="our ranch" />
           </div>
         </div>
       </div>
@@ -119,9 +114,9 @@ export default function Home() {
         <h2
           className={`${titleFont.className} absolute left-[2%] top-[-1rem] ml-[11.75rem] -translate-x-1/2 transform bg-customDarkGreen p-12 text-5xl text-customWhite`}
         >
-         Our Location
+          Our Location
         </h2>
-        <div className="my-44 flex w-4/5 items-center justify-center  rounded bg-customLightGreen p-8">
+        <div className="my-40 flex w-4/5 items-center justify-center  rounded bg-customLightGreen p-8">
           <div className="flex w-1/3 justify-start rounded-[50%]">
             <Image
               src={cottage}
@@ -132,7 +127,7 @@ export default function Home() {
             />
           </div>
           <div className="flex w-3/5 flex-col items-end text-right">
-            <p className="p-4">
+            <p className={`${textFont.className} text-3xl`}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
