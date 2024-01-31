132d62e955f296ad68534c925598ff7d28a4749b
Kelli Libin <km.putnam2@gmail.com>
2024-01-31 14:12:49 -0700

    added box around 1st and 3rd content divs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import { textFont, titleFont } from "../app/utils/fonts";
 import paths from "./paths";
 import Slider from "@/components/Slider";
 import { FaArrowAltCircleRight } from "react-icons/fa";
+import { LiaHatCowboySideSolid } from "react-icons/lia";
+import { GiCow } from "react-icons/gi";
 
 export default function Home() {
   return (
@@ -45,30 +47,32 @@ export default function Home() {
 
       {/* who we are section */}
 
-      <div className="relative mx-auto flex w-full items-center justify-center bg-customWhite">
-          <div
+      <div className="relative mx-auto flex w-full items-center justify-center bg-customBrown bg-opacity-5">
+        <div className=" absolute my-auto w-[97%] rounded h-[83%]  border-2 border-customMediumGreen opacity-10"></div>
+        <div
           className={`${titleFont.className} shadow-element absolute left-[2%] top-[-1rem] ml-[11.5rem] flex -translate-x-1/2 transform flex-col items-center bg-customDarkGreen p-9 text-5xl text-customWhite`}
         >
           <h2 className="text-center">Who We Are</h2>
           <h5
-            className={`${textFont.className} mt-2 w-[full] text-center text-lg tracking-wide`}
+            className={`${textFont.className} mt-2 w-[full] text-center text-lg tracking-wide text-customLightGreen opacity-90`}
           >
             Local and Sustainable
           </h5>
+          
         </div>
-        <div className="shadow-element-grey my-[10rem] flex h-[100%] w-[90%] items-center justify-center rounded bg-customBrown bg-opacity-30 p-6">
-          <div className="shadow-element flex h-[95%] w-[100%] items-center justify-between rounded bg-customLightGreen bg-opacity-60 p-4">
+        <div className=" shadow-element-grey my-[10rem] flex h-[100%] w-[90%] items-center justify-center rounded bg-customBrown bg-opacity-30 p-6">
+          <div className=" shadow-element flex h-[95%] w-[100%] items-center justify-between rounded bg-customLightGreen bg-opacity-60 p-4">
             <div className="flex w-1/3 flex-col items-center justify-center rounded-[50%] py-3">
               <Image
                 src={cottage}
                 height={350}
                 width={350}
                 alt="our ranch"
-                className="shadow-element rounded-[50%] border border-gray-400"
+                className="shadow-element rounded-[50%] border-4 border-customMediumGreen"
               />
               {/* <h4 className={`${titleFont.className} text-lg my-4`}>John & Peggy</h4> */}
             </div>
-            <div className="mx-auto flex w-3/5 flex-col items-end text-right">
+            <div className=" mx-auto flex w-3/5 flex-col items-end text-right">
               <p className={`${textFont.className} p-4 text-3xl leading-snug`}>
                 Discover JPEL Farms, a family-run farm nestled in the scenic
                 landscapes of Colorado. Our passion lies in the meticulous care
@@ -112,13 +116,14 @@ export default function Home() {
       </div>
 
       {/* buy beef section */}
-      <div className="relative mx-auto flex h-full w-full items-center justify-center bg-customWhite">
+      <div className="relative mx-auto flex h-full w-full items-center justify-center bg-customBrown bg-opacity-5">
+      <div className=" absolute my-auto w-[97%] rounded h-[83%]  border-2 border-customMediumGreen opacity-10"></div>
         <div
           className={`${titleFont.className} shadow-element absolute left-[2%] top-[-1rem] ml-[24rem] flex -translate-x-1/2 transform flex-col items-center bg-customDarkGreen p-9 text-5xl text-customWhite`}
         >
           <h2 className="text-center">100% Free-Range, Grassfed Beef</h2>
           <h5
-            className={`${textFont.className} mt-2 w-[50%] text-center text-lg tracking-wide`}
+            className={`${textFont.className} mt-2 w-[50%] text-center text-lg tracking-wide text-customLightGreen opacity-90`}
           >
             Tender and Equisitely Flavorful Cuts
           </h5>
@@ -131,7 +136,7 @@ export default function Home() {
                 height={350}
                 width={350}
                 alt="our ranch"
-                className="shadow-element rounded-[50%] border border-gray-400"
+                className="shadow-element rounded-[50%] border-4 border-customMediumGreen"
               />
             </div>
             <div className="mx-auto flex w-3/5 flex-col items-end text-right">
@@ -146,6 +151,7 @@ export default function Home() {
                 flavor, tenderness, and juiciness. JPEL Farms proudly offers
                 USDA-certified cuts.
               </p>
+
               <Link
                 className={`${titleFont.className} shadow-element-grey mt-4 flex items-center rounded bg-customDarkGreen p-4 tracking-wider text-customWhite
                  underline duration-200 hover:scale-105 hover:bg-customMediumGreen active:scale-90`}
