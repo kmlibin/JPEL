2c7e3e7828d28f0d93fdc5cd7e5cc8914cc2eb50
Kelli Libin <km.putnam2@gmail.com>
2024-02-01 16:36:29 -0700

    put green header in own component, implemented

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { titleFont, textFont } from "@/app/utils/fonts"
+
+interface HeaderProps {
+    header: string,
+    subheader: string,
+    top: string,
+    left: string,
+    ml: string
+    translatex: string
+    transform: string
+}
+
+export default function Header({header, subheader, top, left, ml, translatex, transform}: HeaderProps) {
+    return (
+        <div
+        className={`${titleFont.className} shadow-element absolute  ${left} ${ml} ${translatex} ${transform} ${top} top-[-1rem]  flex  flex-col items-center bg-customDarkGreen p-9 text-5xl text-customWhite`}
+      >
+        <h2 className="text-center">
+          {header}
+        </h2>
+        <h5
+          className={`${textFont.className} mt-2 w-[full] text-center text-lg tracking-wide text-customLightGreen opacity-90`}
+        >
+          {subheader}
+        </h5>
+      </div>
+    )
+}
--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import { titleFont, textFont } from "@/app/utils/fonts";
+import Header from "./Header";
 
 interface HomeCardProps {
   imgSrc: any;
@@ -9,6 +10,11 @@ interface HomeCardProps {
   content: string;
   header: string;
   subheader: string;
+  left: string;
+  top: string;
+  ml: string;
+  translatex: string;
+  transform: string;
 }
 export default function HomeCard({
   imgSrc,
@@ -16,20 +22,25 @@ export default function HomeCard({
   content,
   header,
   subheader,
+  left,
+  top,
+  ml,
+  translatex,
+  transform,
 }: HomeCardProps) {
   return (
     <div className="relative mx-auto flex w-full items-center justify-center bg-customBeige bg-opacity-10">
       <div className=" absolute my-auto h-[83%] w-[97%] rounded  border-2 border-customMediumGreen opacity-20"></div>
-      <div
-        className={`${titleFont.className} shadow-element absolute left-[2%] top-[-1rem] ml-[11.5rem] flex -translate-x-1/2 transform flex-col items-center bg-customDarkGreen p-9 text-5xl text-customWhite`}
-      >
-        <h2 className="text-center">{header}</h2>
-        <h5
-          className={`${textFont.className} mt-2 w-[full] text-center text-lg tracking-wide text-customLightGreen opacity-90`}
-        >
-          {subheader}
-        </h5>
-      </div>
+      <Header
+        header={header}
+        subheader={subheader}
+        ml={ml}
+        left={left}
+        top={top}
+        translatex={translatex}
+        transform={transform}
+      />
+
       <div className=" shadow-element-grey my-[10rem] flex h-[100%] w-[90%] items-center justify-center rounded bg-customBrown bg-opacity-30 p-6">
         <div className=" shadow-element flex h-[95%] w-[100%] items-center justify-between rounded bg-customLightGreen bg-opacity-60 p-4">
           <div className="flex w-1/3 flex-col items-center justify-center rounded-[50%] py-3">
