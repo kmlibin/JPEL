8f82923f8a05ecba12dd4b1a1a9a11896341dd63
Kelli Libin <km.putnam2@gmail.com>
2024-02-02 16:43:07 -0700

    format

--- src/components/GreenHeader.tsx
+++ src/components/GreenHeader.tsx
@@ -8,12 +8,13 @@ interface HeaderProps {
     ml?: string
     translatex?: string
     transform?: string
+   
 }
 
 export default function Header({header, subheader, top, left, ml, translatex, transform}: HeaderProps) {
     return (
         <div
-        className={`${titleFont.className} shadow-element absolute  ${left} ${ml} ${translatex} ${transform} ${top} top-[-1rem]  flex  flex-col items-center bg-customDarkGreen p-9 text-5xl text-customWhite`}
+        className={`${titleFont.className} shadow-element absolute ${left} ${ml} ${translatex} ${transform} ${top} top-[-1rem]  flex  flex-col items-center bg-customDarkGreen p-9 text-5xl text-customWhite`}
       >
         <h2 className="text-center">
           {header}
