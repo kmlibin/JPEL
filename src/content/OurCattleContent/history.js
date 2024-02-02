91d542c5078b318e4913f489aef6b4e40d394fae
Kelli Libin <km.putnam2@gmail.com>
2024-02-02 14:15:53 -0700

    split content into its own file

---
+++ src/content/OurCattleContent/history.js
@@ -0,0 +1,53 @@
+const history = {
+  headerOne: "Scottish Roots",
+  headerTwo: "A Strong and Hardy Breed",
+  headerThree: "Highland Cattle Around the World",
+  contentZero: `Deeply ingrained in the rugged Scottish Highlands for centuries,
+    the Highland Cow underwent a rigorous natural selection process,
+    where only the fittest and most adaptable individuals endured the
+    harsh conditions to carry forth the breed's legacy. Originally
+    classified into two distinct classes, the smaller black Kyloe and
+    the larger red or black Highlander, these strains now amalgamate
+    into a single recognized breed known as Highland.`,
+  contentOne: ` In the 18th-century Scottish economy, these cattle played a
+    pivotal role, with thousands traversing the country's landscapes
+    for markets like Falkirk and Crieff. Queen Victoria's preference
+    for the red-colored cattle led to selective breeding, influencing
+    their predominant hue today.`,
+  contentTwo: ` Highland Cattle exhibit remarkable versatility in
+    agricultural practices, excelling in harsh climates and
+    contributing not only to premium beef but also to milk production
+    and cross-breeding initiatives. Their distinctive double coat, featuring a long, coarse outer
+    layer and a soft wooly undercoat, serves as natural insulation,
+    reducing the necessity for shelter and emphasizing their enduring
+    resilience. Beyond their physical characteristics, Highland Cattle
+    are integral to light grazing practices, promoting environmental
+    sustainability and garnering international recognition for their
+    adaptability. The slow maturation of Highland cattle contributes
+    to meat that is tender, flavorful, and succulent. In a study
+    conducted at the Manyberries Research Station in Canada, Highland
+    beef surpassed other breeds in terms of tenderness and taste.
+    Comparisons with Hereford cattle revealed that the Highland group
+    produced 2000 pounds more beef, while Highland/Hereford crosses
+    outperformed purebred Herefords by 6000 pounds.`,
+  contentThree: `The breed's global significance extends beyond its historical
+  roots.With a robust population of over 40,000 reported worldwide, Highland
+    cattle have become a symbol of adaptability and resilience.  `,
+  contentFour: `They thrive in diverse climates and landscapes, from the cold winters of
+    Norway to the sweeping pastures of Canada. Revered for their exceptional beef quality, lean meat, and unique grazing abilities,
+    Highland cattle continue to play a significant role in agricultural
+    practices globally. As they graze and thrive in various
+    environments, these "Hairy Coos" have not only preserved their rich
+    Scottish heritage but have also become valued contributors to
+    sustainable agriculture and effective range management worldwide`,
+  factOne: ` With archaeological evidence dating back to the 6th century and
+    written records from the 12th century, the Highland Cow (affectionately known in Scotland
+    as the "Hairy Coo) has played a large role in Scottish history"`,
+  factTwo: `Highland beef is renowned for its lower fat and cholesterol
+    content compared to other beef breeds, even surpassing chicken
+    in certain aspects. The meat's fine texture, succulent flavor,
+    and high protein content make it a premium choice in the
+    culinary world`,
+};
+
+export default history;
