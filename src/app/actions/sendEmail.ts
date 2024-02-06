6e7a76285ed591afd453a0fdf9a8210b74643e29
Kelli Libin <km.putnam2@gmail.com>
2024-02-06 13:48:40 -0700

    set up interface type, validations

--- src/app/actions/sendEmail.ts
+++ src/app/actions/sendEmail.ts
@@ -1,89 +1,119 @@
-import { Aoboshi_One } from "next/font/google"
+import { Aoboshi_One } from "next/font/google";
 
 interface sendEmailFormState {
-    errors: {
-        content?: string[],
-        _form?: string[]
-    }
-    success?: boolean
+  errors: {
+    message?: string[];
+    phone?: string[];
+    email?: string[];
+    name?: string[];
+    _form?: string[];
+  };
+  success?: boolean;
 }
 
-export async function sendEmail(formState: sendEmailFormState, formData: FormData): Promise<sendEmailFormState> {
-   const name = formData.get('name')
-   const email = formData.get('email')?.toString()
-   const number = formData.get('number')
-   const message = formData.get('message')
+export async function sendEmail(
+  formState: sendEmailFormState,
+  formData: FormData,
+): Promise<sendEmailFormState> {
+  const name = formData.get("name");
+  const email = formData.get("email")?.toString();
+  const phone = formData.get("phone");
+  const message = formData.get("message");
+  console.log(name, email, phone, message);
+  
+  //validate. if valid put in an object to send to email js. if not valid, set up and send back errors
 
-   //validate. if valid put in an object to send to email js. if not valid, set up and send back errors
+  //needs at least a phone OR email. so
+  if (!phone && !email) {
+    return {
+      errors: {
+        phone: ["Please provide either an email or phone number"],
+        email: ["Please provide either an email or phone number"],
+      },
+    };
+  }
 
-       if (!name) {
-
+  if (!name) {
+    return {
+        errors: {
+            name: ['Please provide a name']
+        }
     }
+  }
 
-    if (!email) {
-      
+  if (email) {
+    const emailPattern = /^\S+@\S+\.\S+$/;
+    const isValidEmail = emailPattern.test(email);
+    if (!isValidEmail) {
+      return {
+        errors: {
+          email: ["Please provide a valid email address"],
+        },
+      };
     }
-
-    if (email) {
-      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-      const isValidEmail = emailPattern.test(email);
-      if (!isValidEmail) {
-    
-      }
+  }
+  if (!message) {
+    return {
+        errors: {
+            message: ['Please write a message to JPEL']
+        }
     }
+  }
 
-    if (!number) {
+  //send to email.js. if success, return no errors. if an issue, return form error
 
-    }
+  //return success or failure
 
-    if (!message) {
-
-    }
-
-    //send to email.js
-
-    //return success or failure
+  return {
+    errors: {},
+    success: true,
+  };
 }
 
-// const validateForm = (formData, setFormErrors) => {
-//     let errors = {};
-//     let formIsValid = true;
+// const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const isValid = validateForm(formData, setFormErrors);
+//     if (isValid) {
+//       try {
+//         emailjs
+//           .sendForm(
+//             process.env.REACT_APP_SERVICE_ID,
+//             "template_m2bhs6m",
+//             form.current,
+//             process.env.REACT_APP_PUBLIC_KEY
+//           )
+//           .then((result) => {
+//             console.log(result.text);
+//             setSubmissionStatus({
+//               heading: "Success",
+//               message:
+//                 "Thanks for your email. Christian will respond within the next 24 hours.",
+//             });
 
-//     if (!formData.name) {
-//       formIsValid = false;
-//       errors["username"] = "*Please enter your name";
-//     }
-
-//     if (!formData.email) {
-//       formIsValid = false;
-//       errors["email"] = "*Please enter your email";
-//     }
-
-//     if (formData.email) {
-//       const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-//       const isValidEmail = emailPattern.test(formData.email);
-//       if (!isValidEmail) {
-//         formIsValid = false;
-//         errors["email"] = "*Please enter a valid email";
+//             setShowModal(true);
+//             setFormData({
+//               name: "",
+//               lastName: "",
+//               email: "",
+//               phone: "",
+//               preferredContactMethod: "phone",
+//               message: "",
+//             });
+//           });
+//       } catch (error) {
+//         setSubmissionStatus({
+//           heading: "Error",
+//           message:
+//             "Error sending email. Please try again later or email email@email directly",
+//         });
+//         setShowModal(true);
 //       }
+//     } else {
+//       setShowModal(true);
+//       setSubmissionStatus({
+//         heading: "Error",
+//         message:
+//           "Form Validation Failed. Please make sure information is correct or email email@email.com",
+//       });
 //     }
-
-//     if (!formData.phone) {
-//       formIsValid = false;
-//       errors["number"] = "*Please enter your phone number";
-//     }
-
-//     if (!formData.preferredContactMethod) {
-//       formIsValid = false;
-//       errors["contactMethod"] = "*Please make a selection";
-//     }
-
-//     if (!formData.message) {
-//       errors["message"] = "*Please provide information";
-//     }
-
-//     setFormErrors(errors);
-//     return formIsValid;
 //   };
-
-//   export default validateForm
