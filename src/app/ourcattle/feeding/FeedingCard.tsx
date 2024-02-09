c43e858b1065f9a215ff456944bc80d4d37d3ee9
Kelli Libin <km.putnam2@gmail.com>
2024-02-09 15:25:02 -0700

    split feeding card info into own component

---
+++ src/app/ourcattle/feeding/FeedingCard.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { titleFont } from "@/app/utils/fonts";
+import Image from "next/image";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { StaticImageData } from "next/image";
+
+interface FeedingCardProps {
+  my?: string;
+  text: {
+    header: string,
+    text: string,
+  }
+  image: StaticImageData;
+  icon: StaticImageData;
+}
+
+const FeedingCard = ({ my, text, image, icon }: FeedingCardProps) => {
+  return (
+    <div
+      className={`${my} shadow-element-light flex w-full items-stretch bg-customMediumGreen bg-opacity-60`}
+    >
+      <div className="min-h-full w-1/2">
+        <Image alt="cow" src={image} className="min-h-full" />
+      </div>
+
+      <div className=" relative flex w-1/2 items-stretch justify-start">
+        <div className="flex h-full w-3/4 flex-col items-center justify-center bg-customLightGreen bg-opacity-70 text-center">
+          <div className="shadow-element-light flex h-[90%] w-[90%] flex-col items-center justify-center rounded bg-customWhite bg-opacity-50">
+            <Image src={icon} alt="corn" height={100} width={100} />
+            <div className="mt-3 flex max-h-full w-full flex-col items-center justify-center">
+              <h3 className={`${titleFont.className} text-3xl`}>
+                {text.header}
+              </h3>
+              <hr className="my-2 w-[70%] border-b border-customBrown opacity-80"></hr>
+              <p className="w-5/6 pt-2 text-[18px]">{text.text}</p>
+            </div>
+          </div>
+        </div>
+        <div className="absolute right-[-3.5rem] top-9 h-full">
+          <FaArrowLeftLong className="text-[10rem] text-customBrown opacity-90" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedingCard;
--- src/app/ourcattle/feeding/page.tsx
+++ src/app/ourcattle/feeding/page.tsx
@@ -2,100 +2,46 @@ import Header from "@/components/GreenHeader";
 import { textFont, titleFont } from "@/app/utils/fonts";
 import Image from "next/image";
 import lukethornton from "../../../../public/images/lukethornton.jpg";
+import lori from "../../../../public/images/lorigrimshaw.jpg";
 import suraang from "../../../../public/images/suraang.png";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import FeedingCard from "./FeedingCard";
+import feedingplan from '../../../content/OurCattleContent/feedingplan'
 
 export default function FeedingPage() {
   return (
     <div className="justify-content relative flex h-full w-full flex-col items-center bg-customBeige bg-opacity-20 pt-[9rem]">
       <div className="h-[90%] w-[95%]">
-        <div className="relative flex h-full w-full flex-col items-center justify-start bg-customBeige bg-opacity-40 pt-4">
+        <div className="relative flex h-full w-full flex-col items-center justify-center bg-customBeige bg-opacity-40">
           <div className=" absolute my-auto h-[97%] w-[97%] rounded  border-2 border-customMediumGreen opacity-20"></div>
           <Header
-            header="Natural Nutrition"
-            subheader="Nutrient-Rich and Natural Diets"
+            header="Our Committment to Quality"
+            subheader="Nutrient-Rich, Natural Diets"
             left="left-14"
           />
 
           <div
-            className={`${textFont.className} shadow-element-light relative mb-[4rem] mt-[11rem] box-content flex w-3/4 flex-col  bg-customWhite bg-opacity-30 p-9  leading-normal tracking-wide`}
+            className={`${textFont.className} shadow-element-light relative mb-[4rem] mt-[11rem] box-content flex h-full w-3/4 flex-col justify-evenly bg-customWhite bg-opacity-30 p-9  leading-normal tracking-wide`}
           >
-            <div className="flex w-full items-stretch">
-              <div className="h-full w-1/2">
-                <Image alt="cow" src={lukethornton} className="min-h-full" />
-              </div>
+            <FeedingCard
+              image={lukethornton}
+              icon={suraang}
+              text={feedingplan.contentOne}
+            />
 
-              <div className=" relative flex w-1/2 items-stretch justify-start bg-customBrown bg-opacity-10">
-                <div className="flex w-3/4 flex-col items-center justify-center bg-slate-400 p-4 text-center">
-                  <Image src={suraang} alt="corn" height={100} width={100} />
-                  <h3 className={`${titleFont.className} text-3xl`}>
-                    No Corn or Soy
-                  </h3>
-                  <p className="w-3/4 py-4">
-                    Feeding corn or soy to cattle deviates from their natural
-                    diet, which can affect their health and wellbeing. It also
-                    makes for poorer quality beef. Corn and soy can also contain
-                    residues of pesticides and herbicides, and you don't want
-                    that in your beef!
-                  </p>
-                </div>
-                <div className="absolute right-[-2rem] top-9 h-full">
-                  <FaArrowLeft className="text-9xl text-customBrown opacity-90" />
-                </div>
-              </div>
-            </div>
+            <FeedingCard
+              my="my-[3.5rem]"
+              image={lukethornton}
+              icon={suraang}
+              text={feedingplan.contentTwo}
+            />
 
-
-            <div className="flex w-full items-stretch my-[3rem]">
-              <div className="h-full w-1/2">
-                <Image alt="cow" src={lukethornton} className="min-h-full" />
-              </div>
-
-              <div className=" relative flex w-1/2 items-stretch justify-start bg-customBrown bg-opacity-10">
-                <div className="flex w-3/4 flex-col items-center justify-center bg-slate-400 p-4 text-center">
-                  <Image src={suraang} alt="corn" height={100} width={100} />
-                  <h3 className={`${titleFont.className} text-3xl`}>
-                    No Corn or Soy
-                  </h3>
-                  <p className="w-3/4 py-4">
-                    Feeding corn or soy to cattle deviates from their natural
-                    diet, which can affect their health and wellbeing. It also
-                    makes for poorer quality beef. Corn and soy can also contain
-                    residues of pesticides and herbicides, and you don't want
-                    that in your beef!
-                  </p>
-                </div>
-                <div className="absolute right-[-2rem] top-9 h-full">
-                  <FaArrowLeft className="text-9xl text-customBrown opacity-90" />
-                </div>
-              </div>
-            </div>
-
-            <div className="flex w-full items-stretch">
-              <div className="h-full w-1/2">
-                <Image alt="cow" src={lukethornton} className="min-h-full" />
-              </div>
-
-              <div className=" relative flex w-1/2 items-stretch justify-start bg-customBrown bg-opacity-10">
-                <div className="flex w-3/4 flex-col items-center justify-center bg-slate-400 p-4 text-center">
-                  <Image src={suraang} alt="corn" height={100} width={100} />
-                  <h3 className={`${titleFont.className} text-3xl`}>
-                    No Corn or Soy
-                  </h3>
-                  <p className="w-3/4 py-4">
-                    Feeding corn or soy to cattle deviates from their natural
-                    diet, which can affect their health and wellbeing. It also
-                    makes for poorer quality beef. Corn and soy can also contain
-                    residues of pesticides and herbicides, and you don't want
-                    that in your beef!
-                  </p>
-                </div>
-                <div className="absolute right-[-2rem] top-9 h-full">
-                  <FaArrowLeft className="text-9xl text-customBrown opacity-90" />
-                </div>
-              </div>
-            </div>
-            
+            <FeedingCard
+              image={lukethornton}
+              icon={suraang}
+              text={feedingplan.contentThree}
+            />
           </div>
         </div>
       </div>
