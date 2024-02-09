bdc47f7b4dc841cd53d66d02a53c3c264603e21d
Kelli Libin <km.putnam2@gmail.com>
2024-02-09 15:25:19 -0700

    moved text content to its own file..need to add icons/imgs

---
+++ src/content/OurCattleContent/feedingplan.js
@@ -0,0 +1,24 @@
+const feedingplan = {
+  contentOne: {
+    header: "No Corn or Soy",
+    text: `Feeding corn or soy to cattle deviates from their natural diet,
+      which can affect their health and wellbeing. It makes for poorer
+      quality beef. Corn and soy can also contain residues of
+      pesticides and herbicides, and you don't want that in your beef!`,
+  },
+  contentTwo: {
+    header: "No Drugs or Chemicals",
+    text: `We never give our cattle chemicals or hormones, resulting in cleaner beef and happier cows. Our commitment 
+    to avoiding chemicals and hormones prioritizes the health and happiness of our cattle, 
+    allowing them to thrive in a stress-free environment.`,
+  },
+
+  contentThree: {
+    header: "100% Grass-fed",
+    text: `Aside from grazing, we provide our cattle a mix of grass, hay, alfalfa, and brewers grain. 
+    This provides year-round consistency in flavor and texture. They also get a free-choice mixture 
+    of unprocessed sea salt and volcanic ash, which provides a full array of minerals `,
+  },
+};
+
+export default feedingplan;
