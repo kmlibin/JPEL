14e64ea5916e3d590ef6ccad238df1c3cf13e474
Kelli Libin <km.putnam2@gmail.com>
2024-02-10 16:09:12 -0700

    put menu in own component, also render freefrom comp

---
+++ src/app/farmbeef/Menu.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Image from 'next/image'
+import supportlocal from '../../../public/images/supportlocal.png'
+import JPELlabel from '../../../public/images/JPELlabel.svg'
+import buybeefcontent from '@/content/BuyBeefContent/buybeefcontent'
+import { titleFont, textFont } from '../utils/fonts'
+
+const Menu = () => {
+  return (
+    <div className="mt-[6%] flex w-full flex-col items-center">
+    <div className="shadow-element flex w-[80%] items-center justify-center rounded bg-customBrown bg-opacity-30">
+      <div className=" shadow-element-light justify-content relative my-[1.5%] flex w-[97%]  items-center justify-center rounded bg-customWhite p-12">
+        <div className="absolute bottom-10 right-10 flex h-[140px] w-[140px] items-center  justify-center rounded-full bg-[#F4ECE4]">
+          <img
+            src={supportlocal.src}
+            className="min-h-[190px] min-w-[190px]"
+          />
+        </div>
+        <div className=" flex h-full w-full items-center justify-center">
+          {/* <div className="absolute top-[0] left-[15%] flex h-[140px] w-[140px] items-center justify-center rounded-full border-2 border-customBrown bg-[#F4ECE4]">
+            <img src={usda.src} className="" />
+          </div> */}
+          <div className=".newwidths mr-2 flex justify-start sm:w-1/2 lg:w-2/3">
+            <Image src={JPELlabel} alt="cow" className="h-auto w-[650px]" />
+          </div>
+          <div className=" shadow-element newwidths box-content flex h-[100%] w-1/3 flex-col items-center justify-center rounded bg-customDarkGreen p-2">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded bg-customLightGreen bg-opacity-90 p-6">
+              <h4 className={`${titleFont.className} mb-4 text-2xl`}>
+                The Menu at JPEL:
+              </h4>
+              <ul
+                className={`${titleFont.className} shadow-element-light w-full rounded bg-customWhite bg-opacity-50 p-4 text-xl leading-normal tracking-wide`}
+              >
+                {buybeefcontent.cutsAndPrices.map(({ cut, price }) => (
+                  <li key={cut}>
+                    <strong>{cut}:</strong>&nbsp;{" "}
+                    <span className={`${textFont.className}`}>{price}</span>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-3 flex flex-col items-start">
+                <p className={`${textFont.className}`}>
+                  *We have many more cuts for purchase, please call for
+                  availability
+                </p>
+                <p className={`${textFont.className} mt-2`}>
+                  *Prices are subject to change
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Menu
--- src/app/farmbeef/page.tsx
+++ src/app/farmbeef/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import seregy2 from "../../../public/images/seregy2.jpg";
-import JPELlabel from "../../../public/images/JPELlabel.svg";
-import beefposter from "../../../public/images/beefposter.png";
-import { titleFont, textFont } from "../utils/fonts";
-import supportlocal from "../../../public/images/supportlocal.png";
+
+import line from "../../../public/images/line.png";
+
 import buybeefcontent from "../../content/BuyBeefContent/buybeefcontent";
 import BeefCard from "@/app/farmbeef/BeefCard";
 import Header from "@/components/GreenHeader";
 import usda from "../../../public/images/usda.png";
+import Menu from "./Menu";
+import FreeFrom from "@/components/FreeFrom/FreeFrom";
 
 export default function FarmBeef() {
   return (
@@ -37,9 +38,9 @@ export default function FarmBeef() {
           <hr className=" border-b border-customMediumGreen"></hr>
           <div className=" absolute -z-10 my-auto mt-[5rem] h-[100%] w-[100%]  rounded border-2 border-customMediumGreen opacity-20"></div>
           {/* 3 content divs */}
-          <div className="mx-2 mt-[12rem] flex w-[97%] flex-col items-center justify-center gap-4 pb-20">
-            <hr className="mb-10 w-[70%] border-b border-customMediumGreen opacity-40"></hr>
-
+          <div className="mx-2 mt-[9rem] flex w-[97%] flex-col items-center justify-center gap-4 pb-20">
+            {/* <hr className="mb-10 w-[70%] border-b border-customMediumGreen opacity-40"></hr> */}
+            <img src={line.src} alt="line" className="mb-10 w-1/3" />
             <div className="flex w-full items-stretch justify-center gap-4">
               {/* TODO put number and color into content file and then map over */}
               <BeefCard
@@ -58,7 +59,8 @@ export default function FarmBeef() {
                 color="bg-customBrown"
               />
             </div>
-            <hr className="mt-10 w-[70%] border-b border-customMediumGreen opacity-40"></hr>
+            <img src={line.src} alt="line" className="mb-[-4rem] mt-10 w-1/3" />
+            {/* <hr className="mt-10 w-[70%] border-b border-customMediumGreen opacity-40"></hr> */}
           </div>
 
           {/* <div className=" absolute my-auto h-[95%] w-[97%] rounded  border-2 border-customMediumGreen opacity-20"></div> */}
@@ -70,55 +72,8 @@ export default function FarmBeef() {
         </div>
       </div>
 
-      {/* TODO split into its own component */}
-      <div className="mt-[6%] flex w-full flex-col items-center">
-        <div className="shadow-element flex w-[80%] items-center justify-center rounded bg-customBrown bg-opacity-30">
-          <div className=" shadow-element-light justify-content relative my-[1.5%] flex w-[97%]  items-center justify-center rounded bg-customWhite p-12">
-            <div className="absolute bottom-10 right-10 flex h-[140px] w-[140px] items-center  justify-center rounded-full bg-[#F4ECE4]">
-              <img
-                src={supportlocal.src}
-                className="min-h-[190px] min-w-[190px]"
-              />
-            </div>
-            <div className=" flex h-full w-full items-center justify-center">
-              {/* <div className="absolute top-[0] left-[15%] flex h-[140px] w-[140px] items-center justify-center rounded-full border-2 border-customBrown bg-[#F4ECE4]">
-                <img src={usda.src} className="" />
-              </div> */}
-              <div className="mr-2 flex justify-start lg:w-2/3 sm:w-1/2">
-                <Image src={JPELlabel} alt="cow" className="h-auto w-[650px]" />
-              </div>
-              <div className=" shadow-element box-content flex h-[100%] w-1/3 flex-col items-center justify-center rounded bg-customDarkGreen p-2">
-                <div className="flex h-full w-full flex-col items-center justify-center rounded bg-customLightGreen bg-opacity-90 p-6">
-                  <h4 className={`${titleFont.className} mb-4 text-2xl`}>
-                    The Menu at JPEL:
-                  </h4>
-                  <ul
-                    className={`${titleFont.className} shadow-element-light w-full rounded bg-customWhite bg-opacity-50 p-4 text-xl leading-normal tracking-wide`}
-                  >
-                    {buybeefcontent.cutsAndPrices.map(({ cut, price }) => (
-                      <li key={cut}>
-                        <strong>{cut}:</strong>&nbsp;{" "}
-                        <span className={`${textFont.className}`}>{price}</span>
-                      </li>
-                    ))}
-                  </ul>
-                  <div className="mt-3 flex flex-col items-start">
-                    <p className={`${textFont.className}`}>
-                      *We have many more cuts for purchase, please call for
-                      availability
-                    </p>
-                    <p className={`${textFont.className} mt-2`}>
-                      *Prices are subject to change
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* add section for all the things the farm is free of */}
+      <Menu />
+      <FreeFrom />
     </div>
   );
 }
