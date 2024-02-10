1bc1b30262d8a77cb7c817d6893b9e1ca23a5ac3
Kelli Libin <km.putnam2@gmail.com>
2024-02-09 17:12:16 -0700

    footer layout and content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,49 @@
 import { BsFacebook } from "react-icons/bs";
+import smallerlogo from "../../public/images/smallerlogo.png";
+import Image from "next/image";
+import { LiaHatCowboySideSolid } from "react-icons/lia";
+import { titleFont, textFont } from "@/app/utils/fonts";
+import { HiOutlineMail } from "react-icons/hi";
+import { MdLocalPhone } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+
 export default function Footer() {
-  
-        return (
-          <footer>
-            <h4 className="">JPEL Farms</h4>
-            <p>address 1</p>
-            <p>address 2</p>
-            <p>1 234 567 8901</p>
-            <br></br>
+  return (
+    <footer className=" box-content flex bg-customBeige bg-opacity-10 border-t-2 border-customBrown border-opacity-10">
+      <div className="w-1/5  px-8 py-3">
+        <Image src={smallerlogo} alt="logo" height={100} width={100} />
+      </div>
+
+      {/* text */}
+      <div
+        className={` ${textFont.className} flex w-4/5 flex-col items-start justify-end py-5`}
+      >
+        <div className="flex items-center">
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center"
+          >
+            <BsFacebook className="mr-3 text-[#007ab2]" />
+            Find us on Facebook!
+          </a>
+
+          <div className="flex items-center">
+            <MdLocalPhone className="ml-5 mr-2 text-customMediumGreen" />
+            <p>1 719 123 2344</p> 
+            <HiOutlineMail className="ml-5 mr-2 text-customMediumGreen" />
             <p>email@email.com</p>
-      
-            <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{marginBottom: "2rem"}}
-            >
-              <BsFacebook />
-            </a>
-      
-            <p>JPEL Farms &copy; 2024</p>
-          </footer>
-        );
-      };
+            <FaLocationDot className="ml-5 mr-2 text-customMediumGreen" />
+            <p>Yoder, Colorado</p>
+          </div>
+        </div>
+
+        <div className="mt-3 flex items-center">
+    
+          <p>JPEL Farms &copy; 2024</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
