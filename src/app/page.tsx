02a273ce53d38eab32270ff3350d426786163231
Kelli Libin <km.putnam2@gmail.com>
2024-02-13 13:52:01 -0700

    display facts card, delete unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
 import cottage from "../../public/images/cottage.jpg";
 import seregykotenev from "../../public/images/seregykotenev.jpg";
-import shanerounce from "../../public/images/shanerounce.jpg";
-import ybagmedia from "../../public/images/ybagmedia.jpg";
-import { textFont, titleFont } from "../app/utils/fonts";
 import paths from "./paths";
-import Slider from "@/components/Home/Slider";
+
 
 import Hero from "@/components/Home/Hero";
 import HomeCard from "@/components/Home/HomeCard";
-import homecontent from '@/content/HomeContent/homecontent.js'
+import homecontent from "@/content/HomeContent/homecontent.js";
+import FactsCard from "@/components/Home/FactsCard";
 
 export default function Home() {
   return (
@@ -33,29 +29,7 @@ export default function Home() {
       />
 
       {/* fun facts section */}
-      <div className=" xlscreen relative flex w-full items-center justify-center lg:h-[110vh]">
-        <div
-          className={`${titleFont.className} shadow-element absolute right-[2%] top-[-1rem] z-10 flex-col items-center bg-customDarkGreen  p-9 text-5xl text-customWhite`}
-        >
-          <h2 className={`${titleFont.className} `}>The Highland Cow</h2>
-          <h5
-            className={`${textFont.className} mt-2 w-[full] text-center text-lg tracking-wide text-customLightGreen opacity-90`}
-          >
-            Shaggy and Adorable
-          </h5>
-        </div>
-        <div className="relative flex h-full w-full items-center justify-end rounded bg-customLightGreen bg-opacity-60">
-          <Slider />
-          <div className="flex h-full justify-end rounded-[50%] lg:w-[85%]">
-            <Image
-              src={shanerounce}
-              className="h-[100%] "
-              style={{ objectFit: "cover" }}
-              alt="our ranch"
-            />
-          </div>
-        </div>
-      </div>
+      <FactsCard />
 
       {/* buy beef section */}
       <HomeCard
