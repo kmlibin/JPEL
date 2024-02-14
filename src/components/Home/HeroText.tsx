9e38e166b5db4037c909a8ab977e2b3d79a24489
Kelli Libin <km.putnam2@gmail.com>
2024-02-14 15:50:47 -0700

    add a delay so not quicker than photo

--- src/components/Home/HeroText.tsx
+++ src/components/Home/HeroText.tsx
@@ -8,17 +8,17 @@ import {Slide, Zoom} from 'react-awesome-reveal';
 const HeroText = () => {
   return (
     <div className="absolute bottom-[18%] right-[5%] flex flex-col items-center text-center">
-    <Slide direction="right" duration={1500} triggerOnce>
+    <Slide direction="right" duration={1500} triggerOnce delay={300}>
       <h1 className={`${titleFont.className} text-6xl text-customWhite`}>
         Quality Mountain-Raised Beef
       </h1>
     </Slide>
-    <Slide direction="left" duration = {1500} triggerOnce>
+    <Slide direction="left" duration = {1500} triggerOnce delay={300}>
     <h3 className={`${titleFont.className} mb-7 text-4xl text-customWhite`}>
       Naturally Organic, Uniquely Flavorful
     </h3>
     </Slide>
-    <Zoom duration={1500} triggerOnce>
+    <Zoom duration={1500} triggerOnce delay={300}>
     <Link
       className={`${textFont.className} shadow-outline flex items-center rounded bg-customBeige p-4 underline duration-300 hover:scale-105 hover:bg-customMediumGreen hover:text-customWhite active:scale-90`}
       href={paths.farmBeefPath()}
